d67ed2233ff316c35547546f5d282ba741b50871
Dilanders <102623317+Dilanders@users.noreply.github.com>
2022-09-14 15:24:10 +0200

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -13,3 +13,22 @@ let supp = document.getElementById('supp');
 supprim.addEventListener('click',()=>{
     supp.remove();
 })
+
+/* ========== Initialize Swiper ========== */
+
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
